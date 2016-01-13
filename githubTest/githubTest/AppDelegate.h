8f1eb2d541e211c67007791ddf6938c0b7c59b22
huangyugui <lan@huangyugui.local>
2016-01-13 15:19:34 +0800

    添加工程

---
+++ githubTest/githubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubTest
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 黄育桂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ githubTest/githubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubTest
+//
+//  Created by lanou on 16/1/13.
+//  Copyright © 2016年 黄育桂. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
